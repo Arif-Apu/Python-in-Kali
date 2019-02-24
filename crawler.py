77da36e2da9c3fa805718b054140277dd91171bc
Arif-Apu <32718654+Arif-Apu@users.noreply.github.com>
2019-02-24 22:54:35 +0100

    crawling a website

---
+++ crawler.py
@@ -0,0 +1,41 @@
+#!usr/bin/env python
+
+import requests
+import re
+import urlparse
+
+'''
+urlparse — Parse URLs into components. ... This module defines a standard interface to break Uniform Resource Locator (URL) strings up in components (addressing scheme, network location, path etc.), to combine the components back into a URL string, and to convert a “relative URL” to an absolute URL given a “base URL.”
+
+'''
+
+target_url = "https://example.com"
+target_links = []
+
+
+def extract_links_from(url):
+    response = requests.get(url)
+    return re.findall('(?:href=")(.*?)"', response.content)
+    #href_links = re.findall('href=".*"', response.content)
+
+
+def crawl(url):
+    href_links = extract_links_from(url)
+    for link in href_links:
+        link = urlparse.urljoin(url, link)
+
+        if "#" in link:
+            link = link.split("#")[0]
+
+        if target_url in link and link not in target_links:
+            target_links.append(link)
+            print(link)
+            crawl(link)
+
+
+crawl(target_url)
+
+
+
+
+
